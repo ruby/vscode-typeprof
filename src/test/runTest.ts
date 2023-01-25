8886be00662f958c4407cde0e287ca230c9974ac
Naoto Ono <onoto1998@gmail.com>
2023-01-25 23:54:36 +0900

    Use two spaces instead of tabs

--- src/test/runTest.ts
+++ src/test/runTest.ts
@@ -3,23 +3,23 @@ import * as path from 'path';
 import { runTests } from '@vscode/test-electron';
 
 async function main() {
-	try {
-		// The folder containing the Extension Manifest package.json
-		// Passed to `--extensionDevelopmentPath`
-		const extensionDevelopmentPath = path.resolve(__dirname, '../../../');
+  try {
+    // The folder containing the Extension Manifest package.json
+    // Passed to `--extensionDevelopmentPath`
+    const extensionDevelopmentPath = path.resolve(__dirname, '../../../');
 
-		// The path to test runner
-		// Passed to --extensionTestsPath
-		const extensionTestsPath = path.resolve(__dirname, './suite/index');
-		const projectRoot = path.join(__dirname, '..', '..', '..');
-		const simpleProgramPath = path.join(projectRoot, 'src', 'test', 'simpleProgram');
+    // The path to test runner
+    // Passed to --extensionTestsPath
+    const extensionTestsPath = path.resolve(__dirname, './suite/index');
+    const projectRoot = path.join(__dirname, '..', '..', '..');
+    const simpleProgramPath = path.join(projectRoot, 'src', 'test', 'simpleProgram');
 
-		// Download VS Code, unzip it and run the integration test
-		await runTests({ extensionDevelopmentPath, extensionTestsPath, launchArgs: [simpleProgramPath] });
-	} catch (err) {
-		console.error('Failed to run tests');
-		process.exit(1);
-	}
+    // Download VS Code, unzip it and run the integration test
+    await runTests({ extensionDevelopmentPath, extensionTestsPath, launchArgs: [simpleProgramPath] });
+  } catch (err) {
+    console.error('Failed to run tests');
+    process.exit(1);
+  }
 }
 
 main();
